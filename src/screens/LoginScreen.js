9b06efac37deba4a518f56429bdb25b3f32121e1
sudeshlak <swelikotuwa@gmail.com>
2021-10-15 00:48:09 +0530

    complete logon navigation

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -6,53 +6,54 @@ import FormButton from '../components/FormButton';
 
 const styles = StyleSheet.create({
     container: {
-      backgroundColor: '#f5f5f5',
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
+        backgroundColor: '#f5f5f5',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     titleText: {
-      fontSize: 24,
-      marginBottom: 10
+        fontSize: 24,
+        marginBottom: 10
     },
     loginButtonLabel: {
-      fontSize: 22
+        fontSize: 22
     },
     navButtonText: {
-      fontSize: 16
+        fontSize: 16
     }
-  });
+});
 
-export default function Login() {
+export default function LoginScreen({ navigation }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-  
+
     return (
-      <View style={styles.container}>
-        <Title style={styles.titleText}>Welcome to Chat app</Title>
-        <FormInput
-          labelName="Email"
-          value={email}
-          autoCapitalize="none"
-          onChangeText={userEmail => setEmail(userEmail)}
-        />
-        <FormInput
-          labelName="Password"
-          value={password}
-          secureTextEntry={true}
-          onChangeText={userPassword => setPassword(userPassword)}
-        />
-        <FormButton
-          title="Login"
-          modeValue="contained"
-          labelStyle={styles.loginButtonLabel}
-        />
-        <FormButton
-          title="New user? Join here"
-          modeValue="text"
-          uppercase={false}
-          labelStyle={styles.navButtonText}
-        />
-      </View>
+        <View style={styles.container}>
+            <Title style={styles.titleText}>Welcome to Chat app</Title>
+            <FormInput
+                labelName="Email"
+                value={email}
+                autoCapitalize="none"
+                onChangeText={userEmail => setEmail(userEmail)}
+            />
+            <FormInput
+                labelName="Password"
+                value={password}
+                secureTextEntry={true}
+                onChangeText={userPassword => setPassword(userPassword)}
+            />
+            <FormButton
+                title="Login"
+                modeValue="contained"
+                labelStyle={styles.loginButtonLabel}
+            />
+            <FormButton
+                title="New user? Join here"
+                modeValue="text"
+                uppercase={false}
+                labelStyle={styles.navButtonText}
+                onPress={() => navigation.navigate('Signup')}
+            />
+        </View>
     );
-  }
+}
