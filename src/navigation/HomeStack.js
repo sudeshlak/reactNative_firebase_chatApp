f308c1bbe1f3c24c8ea55b924c05ab9c1575f350
sudeshlak <swelikotuwa@gmail.com>
2021-10-17 12:05:49 +0530

    create chat room completed

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import AddRoomScreen from '../screens/AddRoomScreen';
+import { IconButton } from 'react-native-paper';
 
-const Stack = createStackNavigator();
+const ChatAppStack = createStackNavigator();
+const ModalStack = createStackNavigator();
+
+function ChatApp() {
+    return (
+        <ChatAppStack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#6646ee'
+                },
+                headerTintColor: '#ffffff',
+                headerTitleStyle: {
+                    fontSize: 22
+                }
+            }}
+        >
+            <ChatAppStack.Screen 
+                name="Home"    
+                component={HomeScreen} 
+                options={({ navigation }) => ({
+                headerRight: () => (
+                    <IconButton
+                        icon="message-plus"
+                        size={28}
+                        color="#ffffff"
+                        onPress={() => navigation.navigate('AddRoom')}
+                    />
+                )
+            })} />
+        </ChatAppStack.Navigator>
+    );
+}
 
 export default function HomeStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
-  );
+    return (
+        <ModalStack.Navigator>
+            <ModalStack.Screen name="ChatApp" component={ChatApp} />
+            <ModalStack.Screen name="AddRoom" component={AddRoomScreen} />
+        </ModalStack.Navigator>
+    );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,10 @@
-
 import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Title } from 'react-native-paper';
 import { AuthContext } from '../navigation/AuthProvider';
 import FormButton from '../components/FormButton';
 
-export default function HomeScreen() {
+export default function HomeScreen({navigation}) {
   const { user, logout } = useContext(AuthContext);
 
   return (
